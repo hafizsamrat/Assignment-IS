48d6ef7927b723567371131b1ff565dd74f02ddb
MD. Hafizur Rahman <hafizur.rahman@selisegroup.com>
2024-07-14 08:47:04 +0600

    code refactored

--- AssignmentIS/OldPhone.cs
+++ AssignmentIS/OldPhone.cs
@@ -3,7 +3,11 @@
 namespace AssignmentIS
 {
     public class OldPhone
-    {
+    {/// <summary>
+     /// The dictionary mapping each keypad character to its corresponding letters.
+     /// </summary>
+        private static Dictionary<char, string> KeyPadMapping = GetKeyPadMapping();
+
         /// <summary>
         /// Converts the input text to its old phone keypad equivalent.
         /// </summary>
@@ -11,52 +15,43 @@ namespace AssignmentIS
         /// <returns>The converted output based on old phone keypad mapping.</returns>
         public static string OldPhonePad(string input)
         {
-            try
+            var output = new StringBuilder();
+            char currentInput = '\0';
+            int currentCount = 0;
+            var actionKeys = new List<char> { '*', '#', ' ' };
+
+            foreach (char ch in input)
             {
-                var output = new StringBuilder();
-                var keyPad = GetKeyPadMapping();
-                char currentInput = '\0';
-                int currentCount = 0;
-                var actionKeys = new List<char> { '*', '#', ' ' };
-
-                foreach (char ch in input)
+                if (actionKeys.Contains(ch))
                 {
-                    if (actionKeys.Contains(ch))
+                    if (currentCount > 0)
                     {
-                        if (currentCount > 0)
-                        {
-                            output.Append(GetCharFromKeypad(currentCount, currentInput, keyPad));
-                            currentCount = 0;
-                        }
-
-                        // Handles special actions based on the action key.
-                        if (ch == '*')
-                        {
-                            if (output.Length > 0) output.Remove(output.Length - 1, 1);
-                        }
-                        else if (ch == '#')
-                        {
-                            break; // Stops processing at the end character '#'
-                        }
+                        output.Append(GetCharFromKeypad(currentCount, currentInput));
+                        currentCount = 0;
                     }
-                    else
+
+                    // Handles special actions based on the action key.
+                    if (ch == '*')
                     {
-                        if (currentCount > 0 && ch != currentInput)
-                        {
-                            output.Append(GetCharFromKeypad(currentCount, currentInput, keyPad));
-                            currentCount = 0;
-                        }
-                        currentInput = ch;
-                        currentCount++;
+                        if (output.Length > 0) output.Remove(output.Length - 1, 1);
+                    }
+                    else if (ch == '#')
+                    {
+                        break; // Stops processing at the end character '#'
                     }
                 }
-                return output.ToString();
+                else
+                {
+                    if (currentCount > 0 && ch != currentInput)
+                    {
+                        output.Append(GetCharFromKeypad(currentCount, currentInput));
+                        currentCount = 0;
+                    }
+                    currentInput = ch;
+                    currentCount++;
+                }
             }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            return string.Empty;
+            return output.ToString();
         }
 
         /// <summary>
@@ -64,11 +59,10 @@ namespace AssignmentIS
         /// </summary>
         /// <param name="currentCount">The count of consecutive occurrences of the input character.</param>
         /// <param name="currentInput">The current input character being processed.</param>
-        /// <param name="keyPad">The dictionary mapping each keypad character to its corresponding letters.</param>
         /// <returns>The character from the keypad mapping based on the current count and input character.</returns>
-        private static char GetCharFromKeypad(int currentCount, char currentInput, Dictionary<char, string> keyPad)
+        private static char GetCharFromKeypad(int currentCount, char currentInput)
         {
-            var currentKeypad = keyPad[currentInput];
+            var currentKeypad = KeyPadMapping[currentInput];
             var len = currentKeypad.Length;
             var index = (currentCount - 1) % len;
             return currentKeypad[index];
